61150db2c78ffda179d2b89b816fa9837c6417f4
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2023-02-18 00:34:26 +0200

    Add minimax algorithm

---
+++ Minimax/Minimax.cs
@@ -0,0 +1,85 @@
+﻿using System.Numerics;
+using System.Xml.Linq;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Minimax
+{
+	public class Minimax
+	{
+		public int GetMiniMax(char[,] board, char forWho)
+		{
+			var score = CheckWhoWIns(board, forWho);
+			if (score != 0)
+			{
+				return score;
+			}
+
+			if (forWho == 'o')
+			{
+				var bestScore = int.MinValue;
+				for (var i = 0; i < 3; i++)
+				{
+					for (var j = 0; j < 3; j++)
+					{
+						if (board[i, j] == '-')
+						{
+							board[i, j] = forWho;
+							var currentScore = GetMiniMax(board, 'x');
+							board[i, j] = '-';
+
+							bestScore = Math.Max(bestScore, currentScore);
+						}
+					}
+				}
+				return bestScore;
+			}
+			else
+			{
+				var bestScore = int.MaxValue;
+				for (var i = 0; i < 3; i++)
+				{
+					for (var j = 0; j < 3; j++)
+					{
+						if (board[i, j] == '-')
+						{
+							board[i, j] = forWho;
+							var currentScore = GetMiniMax(board, 'o');
+							board[i, j] = '-';
+
+							bestScore = Math.Min(bestScore, currentScore);
+						}
+					}
+				}
+				return bestScore;
+			}
+		}
+
+		private int CheckWhoWIns(char[,] board, char forWho)
+		{
+			if ((board[0, 0] == forWho && board[0, 1] == forWho && board[0, 2] == forWho)
+		|| (board[1, 0] == forWho && board[1, 1] == forWho && board[1, 2] == forWho)
+		|| (board[2, 0] == forWho && board[2, 1] == forWho && board[2, 2] == forWho)
+		|| (board[0, 0] == forWho && board[1, 0] == forWho && board[2, 0] == forWho)
+		|| (board[0, 1] == forWho && board[1, 1] == forWho && board[2, 1] == forWho)
+		|| (board[0, 2] == forWho && board[1, 2] == forWho && board[2, 2] == forWho)
+		|| (board[0, 0] == forWho && board[1, 1] == forWho && board[2, 2] == forWho)
+		|| (board[0, 2] == forWho && board[1, 1] == forWho && board[2, 0] == forWho))
+			{
+				var score = 1;
+				for (var i = 0; i < 3; i++)
+				{
+					for (var j = 0; j < 3; j++)
+					{
+						if (board[i, j] == '-')
+						{
+							score++;
+						}
+					}
+				}
+				return score;
+			}
+			else
+				return 0;
+		}
+	}
+}
---
+++ Minimax/Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace Minimax
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var minimax = new Minimax();
+
+			char[,] board = { { '-', 'o', 'x'},
+							  { '-', '-', '-'},
+							  { 'x', '-', '-'} };
+
+			//INPUT
+			//  '-', 'o', 'x'
+			//  '-', 'o', '-'
+			//  'x', '-', '-'
+
+			//OUTPUT
+			// '-' 'o' 'x'
+			// '-' 'o' '-'
+			// 'x' '-' 'x'
+
+			var bestScore = int.MinValue;
+			var moveRow = -1;
+			var moveCol = -1;
+
+			for (int row = 0; row < board.GetLength(0); row++)
+			{
+				for (int col = 0; col < board.GetLength(1); col++)
+				{
+					if (board[row, col] == '-')
+					{
+						var score = minimax.GetMiniMax(board, 'x');
+
+						board[row, col] = '-';
+
+						Console.WriteLine("Score: " + score);
+						Console.WriteLine("BestScore: " + bestScore);
+
+						if (score > bestScore)
+						{
+							moveRow = row;
+							moveCol = col;
+						}
+					}
+				}
+
+			}
+			board[moveRow, moveCol] = 'x';
+
+			for (int row = 0; row < board.GetLength(0); row++)
+			{
+				for (int col = 0; col < board.GetLength(1); col++)
+				{
+					Console.Write($"'{board[row, col]}' ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
